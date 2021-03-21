b7486e7b591e5dc4c02bf4228b18a24c665a1749
mujiindriani <67526113+mujiindriani@users.noreply.github.com>
2021-03-21 14:26:45 +0700

    Add files via upload

---
+++ jadwal.php
@@ -0,0 +1,430 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="jadwal.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+</head>
+<body>
+<nav class="navbar navbar-light bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="landpage.php"><img src="images/19.png" style="width:50px; height:50px;" alt=""></a>
+    
+
+    <form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<div class="menu">
+	<div class="logo">
+		<dl>
+			<dt><img src="images/1.png" width= 100 px> </dt>
+			<dt><label><h1>AREX</h1></label></dt>
+		</dl>
+	</div>
+	<div class="list">
+		<dl >
+			<dt class="pelni"> Kapal Pelni </dt>
+                <dd><a 	class="link" href="#K1">- Kapal Pelni Tidar </a></dd>
+				<dd><a  class="link" href="#K2">- Kapal Pelni Binaiya </a></dd>
+				<dd><a  class="link" href="#K3">- Kapal Pelni Awu </a></dd>
+				<dd><a  class="link" href="#K4">- Kapal Pelni Lawit </a></dd>
+				<dd><a 	class="link" href="#K5">- Kapal Pelni Dorolonda </a></dd>
+				<dd><a  class="link" href="#K6">- Kapal Pelni Bukit Raya </a></dd>
+				<dd><a  class="link" href="#K7">- Kapal Pelni Labobar </a></dd>
+				<dd><a  class="link" href="#K8">- Kapal Pelni Lambelu</a></dd>
+		</dl>
+	</div>
+</div>
+<div class="home">
+	<div class="title">
+	<label ><h2> Jadwal Kapal Pelayaran Nasional Indonesia (PELNI) </h2></label>
+	</div>
+	<div class="page">
+		<table>
+			<tr>
+				<th colspan="3" class="kapal" id="K1"> Jadwal Kapal Tidar </th>
+			</tr>
+			<tr>
+				<th class="rute"> Rute </th>
+				<th class="berangkat"> Berangkat </th>
+				<th class="Tiba"> Tiba </th>
+			</tr>
+			<tr>
+				<td> Namlea (Buru) - Ambon </td>
+				<td> 22 Februari 2021, 12:00 </td>
+				<td> 22 Februari 2021, 17:00 </td>
+			</tr>
+			<tr>
+				<td> Ambon - Tual </td>
+				<td> 22 Februari 2021, 21:00 </td>
+				<td> 23 Februari 2021, 16:00 </td>
+			</tr>
+			<tr>
+				<td> Tual - Dobo </td>
+				<td> 23 Februari 2021, 20:00 </td>
+				<td> 24 Februari 2021, 03:00 </td>
+			</tr>
+			<tr>
+				<td> Dobo - Kaimana </td>
+				<td> 24 Februari 2021, 06:00 </td>
+				<td> 24 Februari 2021, 14:00 </td>
+			</tr>
+			<tr>
+				<td> Kaimana - Fak-Fak </td>
+				<td> 24 Februari 2021, 17:00 </td>
+				<td> 25 Februari 2021, 03:00 </td>
+			</tr>
+			<tr>
+				<td> Fak-Fak - Sorong </td>
+				<td> 25 Februari 2021, 07:00 </td>
+				<td> 26 Februari 2021, 21:00 </td>
+			</tr>
+			<tr>
+				<td> Sorong - Manokwari </td>
+				<td> 26 Februari 2021, 02:00 </td>
+				<td> 26 Februari 2021, 15:00 </td>
+			</tr>
+			<tr>
+				<td> Manokwari - Sorong </td>
+				<td> 26 Februari 2021, 19:00 </td>
+				<td> 27 Februari 2021, 08:00 </td>
+			</tr>
+			<tr>
+				<td> Sorong - Fak-Fak </td>
+				<td> 27 Februari 2021, 12:00 </td>
+				<td> 28 Februari 2021, 02:00 </td>
+			</tr>
+			<tr>
+				<td> Fak-Fak - Kaimana </td>
+				<td> 27 Februari 2021, 05:00 </td>
+				<td> 28 Februari 2021, 12:00 </td>
+			</tr>
+			<tr>
+				<td> Kaimana - Dobo </td>
+				<td> 28 Februari 2021, 16:00 </td>
+				<td> 01 Maret 2021, 00:01 </td>
+			</tr>
+			<tr>
+				<td> Dobo - Tual </td>
+				<td> 01 Maret 2021, 02:00 </td>
+				<td> 01 Maret 2021, 09:00 </td>
+			</tr>
+			<tr>
+				<td> Tual - Ambon </td>
+				<td> 01 Maret 2021, 12:00 </td>
+				<td> 02 Maret 2021, 07:00 </td>
+			</tr>
+			<tr>
+				<td> Ambon - Namlea (Buru) </td>
+				<td> 02 Maret 2021, 10:00 </td>
+				<td> 02 Maret 2021, 15:00 </td>
+			</tr>
+			<tr>
+				<th colspan="3" class="kapal" id="K2"> Jadwal Kapal Binaiya </th>
+			</tr>
+			<tr>
+				<th class="rute"> Rute </th>
+				<th class="berangkat"> Berangkat </th>
+				<th class="Tiba"> Tiba </th>
+			</tr>
+			<tr>
+				<td> Bima - Labuan Bajo </td>
+				<td> 02 Februari 2021, 08:00 </td>
+				<td> 02 Februari 2021, 16:00 </td>
+			</tr>
+			<tr>
+				<td> Labuan Bajo - Makassar </td>
+				<td> 02 Februari 2021, 20:00 </td>
+				<td> 03 Februari 2021, 16:00 </td>
+			</tr>
+			<tr>
+				<td> Makassar - Labuan Bajo </td>
+				<td> 03 Februari 2021, 23:00 </td>
+				<td> 04 Februari 2021, 20:00 </td>
+			</tr>
+			<tr>
+				<td> Labuan Bajo - Bima </td>
+				<td> 04 Februari 2021, 22:00 </td>
+				<td> 05 Februari 2021, 06:00 </td>
+			</tr>
+			<tr>
+				<td> Bima - Benoa (Denpasar) </td>
+				<td> 05 Februari 2021, 08:00 </td>
+				<td> 06 Februari 2021, 07:00 </td>
+			</tr>
+			<tr>
+				<td> Benoa (Denpasar) - Labuan Bajo </td>
+				<td> 07 Februari 2021, 08:00 </td>
+				<td> 08 Februari 2021, 14:00 </td>
+			</tr>
+			<tr>
+				<td> Labuan Bajo - Benoa (Denpasar) </td>
+				<td> 08 Februari 2021, 16:00 </td>
+				<td> 09 Februari 2021, 22:00 </td>
+			</tr>
+			<tr>
+				<th colspan="3" class="kapal" id="K3"> Jadwal Kapal Awu </th>
+			</tr>
+			<tr>
+				<th class="rute"> Rute </th>
+				<th class="berangkat"> Berangkat </th>
+				<th class="Tiba"> Tiba </th>
+			</tr>
+			<tr>
+				<td> Benoa (Denpasar) - Bima </td>
+				<td> 05 Februari 2021, 20:00 </td>
+				<td> 06 Februari 2021, 19:00 </td>
+			</tr>
+			<tr>
+				<td> Bima - Waingapu (Sumba Timur) </td>
+				<td> 06 Februari 2021, 21:00 </td>
+				<td> 07 Februari 2021, 12:00 </td>
+			</tr>
+			<tr>
+				<td> Waingapu (Sumba Timur) - Ende </td>
+				<td> 07 Februari 2021, 15:00 </td>
+				<td> 08 Februari 2021, 01:00 </td>
+			</tr>
+			<tr>
+				<td> Ende - Tenau (Kupang) </td>
+				<td> 08 Februari 2021, 03:00 </td>
+				<td> 08 Februari 2021, 18:00 </td>
+			</tr>
+			<tr>
+				<td> Tenau (Kupang) - Kalabahi </td>
+				<td> 08 Februari 2021, 21:00 </td>
+				<td> 09 Februari 2021, 11:00 </td>
+			</tr>
+			<tr>
+				<td> Kalabahi - Tenau (Kupang)</td>
+				<td> 09 Februari 2021, 13:00 </td>
+				<td> 10 Februari 2021, 03:00 </td>
+			</tr>
+			<tr>
+				<td> Tenau (Kupang) - Ende </td>
+				<td> 10 Februari 2021, 05:00 </td>
+				<td> 10 Februari 2021, 20:00 </td>
+			</tr>
+			<tr>
+				<td> Ende - Waingapu (Sumba Timur) </td>
+				<td> 10 Februari 2021, 22:00 </td>
+				<td> 11 Februari 2021, 09:00 </td>
+			</tr>
+			<tr>
+				<td> Waingapu (Sumba Timur) - Bima </td>
+				<td> 11 Februari 2021, 11:00 </td>
+				<td> 12 Februari 2021, 02:00 </td>
+			</tr>
+			<tr>
+				<td> Bima - Benoa (Denpasar) </td>
+				<td> 12 Februari 2021, 04:00 </td>
+				<td> 13 Februari 2021, 03:00 </td>
+			</tr>
+			<tr>
+				<th colspan="3" class="kapal" id="K4"> Jadwal Kapal Lawit </th>
+			</tr>
+			<tr>
+				<th class="rute"> Rute </th>
+				<th class="berangkat"> Berangkat </th>
+				<th class="Tiba"> Tiba </th>
+			</tr>
+			<tr>
+				<td> Tanjung Emas (Semarang) - Kumai (Kotawaringin) </td>
+				<td> 05 Februari 2021, 13:00 </td>
+				<td> 06 Februari 2021, 15:00 </td>
+			</tr>
+			<tr>
+				<td> Kumai (Kotawaringin) - Tanjung Emas (Semarang) </td>
+				<td> 06 Februari 2021, 17:00 </td>
+				<td> 07 Februari 2021, 18:00 </td>
+			</tr>
+			<tr>
+				<td> Tanjung Emas (Semarang) - Pontianak </td>
+				<td> 08 Februari 2021, 09:00 </td>
+				<td> 10 Februari 2021, 06:00 </td>
+			</tr>
+			<tr>
+				<td> Pontianak - Tanjung Priok (Jakarta) </td>
+				<td> 10 Februari 2021, 08:00 </td>
+				<td> 12 Februari 2021, 05:00 </td>
+			</tr>
+			<tr>
+				<td> Tanjung Priok (Jakarta) - Pontianak </td>
+				<td> 13 Februari 2021, 15:00 </td>
+				<td> 15 Februari 2021, 09:00 </td>
+			</tr>
+			<tr>
+				<td> Pontianak - Tanjung Emas (Semarang) </td>
+				<td> 15 Februari 2021, 11:00 </td>
+				<td> 17 Februari 2021, 08:00 </td>
+			</tr>
+			<tr>
+				<td> Tanjung Emas (Semarang) - Kumai (kotawaringin Barat) </td>
+				<td> 19 Februari 2021, 13:00 </td>
+				<td> 20 Februari 2021, 20:00 </td>
+			</tr>
+			<tr>
+				<td> Kumai (kotawaringin Barat) - Tanjung Emas (Semarang) </td>
+				<td> 20 Februari 2021, 17:00 </td>
+				<td> 21 Februari 2021, 18:00 </td>
+			</tr>
+			<tr>
+				<td> Tanjung Emas (Semarang) - Pontianak </td>
+				<td> 22 Februari 2021, 09:00 </td>
+				<td> 24 Februari 2021, 06:00 </td>
+			</tr>
+			<tr>
+				<td> Pontianak - Tanjung Priok (Jakarta) </td>
+				<td> 24 Februari 2021, 08:00 </td>
+				<td> 26 Februari 2021, 05:00 </td>
+			</tr>
+			<tr>
+				<th colspan="3" class="kapal" id="K5"> Jadwal Kapal Dorolando </th>
+			</tr>
+			<tr>
+				<th class="rute"> Rute </th>
+				<th class="berangkat"> Berangkat </th>
+				<th class="Tiba"> Tiba </th>
+			</tr>
+			<tr>
+				<td> Tanjung Priok (Jakarta) - Tanjung Perak (Surabaya) </td>
+				<td> 01 Februari 2021, 15:00 </td>
+				<td> 02 Februari 2021, 16:00 </td>
+			</tr>
+			<tr>
+				<td> Tanjung Perak (Surabaya) - Makassar </td>
+				<td> 02 Februari 2021, 21:00 </td>
+				<td> 04 Februari 2021, 02:00 </td>
+			</tr>
+			<tr>
+				<td> Makassar - Bau-Bau </td>
+				<td> 04 Februari 2021, 06:00 </td>
+				<td> 04 Februari 2021, 20:00 </td>
+			</tr>
+			<tr>
+				<td> Bau-Bau - Namlea (Buru) </td>
+				<td> 04 Februari 2021, 22:00 </td>
+				<td> 05 Februari 2021, 21:00 </td>
+			</tr>
+			<tr>
+				<td> Namlea (Buru) - Ambon </td>
+				<td> 05 Februari 2021, 23:00 </td>
+				<td> 06 Februari 2021, 04:00 </td>
+			</tr>
+			<tr>
+				<th colspan="3" class="kapal" id="K6"> Jadwal Kapal Bukit Raya </th>
+			</tr>
+			<tr>
+				<th class="rute"> Rute </th>
+				<th class="berangkat"> Berangkat </th>
+				<th class="Tiba"> Tiba </th>
+			</tr>
+			<tr>
+				<td> Tanjung Perak (Surabaya) - Pontianak </td>
+				<td> 05 Februari 2021, 03:00 </td>
+				<td> 07 Februari 2021, 02:00 </td>
+			</tr>
+			<tr>
+				<td> Pontianak - Serasan </td>
+				<td> 07 Februari 2021, 04:00 </td>
+				<td> 07 Februari 2021, 18:00 </td>
+			</tr>
+			<tr>
+				<td> Serasan - Midai </td>
+				<td> 07 Februari 2021, 19:00 </td>
+				<td> 08 Februari 2021, 03:00 </td>
+			</tr>
+			<tr>
+				<td> Midai - Natuna </td>
+				<td> 08 Februari 2021, 05:00 </td>
+				<td> 08 Februari 2021, 09:00 </td>
+			</tr>
+			<tr>
+				<td> Natuna - Tarempa </td>
+				<td> 08 Februari 2021, 10:00 </td>
+				<td> 08 Februari 2021, 20:00 </td>
+			</tr>
+			<tr>
+				<th colspan="3" class="kapal" id="K7"> Jadwal Kapal Labobar </th>
+			</tr>
+			<tr>
+				<th class="rute"> Rute </th>
+				<th class="berangkat"> Berangkat </th>
+				<th class="Tiba"> Tiba </th>
+			</tr>
+			<tr>
+				<td> Serui - Jayapura </td>
+				<td> 02 Februari 2021, 13:00 </td>
+				<td> 03 Februari 2021, 08:00 </td>
+			</tr>
+			<tr>
+				<td> Jayapura - Serui </td>
+				<td> 03 Februari 2021, 12:00 </td>
+				<td> 04 Februari 2021, 07:00 </td>
+			</tr>
+			<tr>
+				<td> Serui - Manokwari </td>
+				<td> 04 Februari 2021, 09:00 </td>
+				<td> 04 Februari 2021, 18:00 </td>
+			</tr>
+			<tr>
+				<td> Manokwari - Sorong </td>
+				<td> 04 Februari 2021, 20:00 </td>
+				<td> 05 Februari 2021, 08:00 </td>
+			</tr>
+			<tr>
+				<td> Sorong - Ternate </td>
+				<td> 05 Februari 2021, 11:00 </td>
+				<td> 06 Februari 2021, 06:00 </td>
+			</tr>
+			<tr>
+				<th colspan="3" class="kapal" id="K8"> Jadwal Kapal Lambelu </th>
+			</tr>
+			<tr>
+				<th class="rute"> Rute </th>
+				<th class="berangkat"> Berangkat </th>
+				<th class="Tiba"> Tiba </th>
+			</tr>
+			<tr>
+				<td> Makassar - Pare-Pare </td>
+				<td> 14 Februari 2021, 14:00 </td>
+				<td> 14 Februari 2021, 20:00 </td>
+			</tr>
+			<tr>
+				<td> Pare-Pare - Balikpapan </td>
+				<td> 14 Februari 2021, 23:00 </td>
+				<td> 15 Februari 2021, 15:00 </td>
+			</tr>
+			<tr>
+				<td> Balikpapan - Tarakan </td>
+				<td> 15 Februari 2021, 18:00 </td>
+				<td> 16 Februari 2021, 21:00 </td>
+			</tr>
+			<tr>
+				<td> Tarakan - Nunukan</td>
+				<td> 17 Februari 2021, 02:00 </td>
+				<td> 17 Februari 2021, 08:00 </td>
+			</tr>
+			<tr>
+				<td> Nunukan - Pantoloan (Donggala) </td>
+				<td> 17 Februari 2021, 11:00 </td>
+				<td> 18 Februari 2021, 08:00 </td>
+			</tr>
+		</table>
+	</div>
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+
+</body>
+</html>
---
+++ logistic.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+</head>
+<body>
+<nav class="navbar navbar-light bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="landpage.php"><img src="images/19.png" style="width:50px; height:50px;" alt=""></a>
+    
+
+    <form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+		
+
+
+<!---left right image-->
+<section id="one" class="wrapper style1" style="margin:10px 50px 200px 50px; background-color:#FFF0F5;">
+				<div class="inner">
+					<article style="margin-bottom:100px;">
+						<span class="feature left" ><img src="images/21.jpg" class="rounded float-start" alt=""  style="width:500px; height:300px;"/></span>
+						<div class="content">
+							<h2 class="text">Informasi Mitra Logistic Pelabuhan</h2>
+							<p class="text">Mitra logistic sangat diperlukan utntuk pengiriman barang melaui jalur laut.<br>Berikut ini merupakan mitra logistic yang tersedia di pelabuhan.</p>
+									<a href="#fat" class="btn btn-secondary btn-lg" style="margin-left:350px;" >Go</a>		
+						</div>
+					</article>
+					<article >
+						<span class="feature right"><img   class="rounded float-end" src="images/20.jpg" alt=""  style="width:500px; height:300px;" /></span>
+						<div class="content">
+							<h2 class="text">Informasi Managemant Gudang Logistic</h2>
+							<p class="text">Dalam pengiriman barang melalui jalur laut ada beberapa hal penting yang harus diperhatikan,
+              untuk itu klik tombol dinbawah untuk mengetahui tata cara pengiriman barang melalui jalur laut.</p>
+									<a href="#mdo" class="btn btn-secondary btn-lg" style="margin-left:350px;">Go</a>
+						</div>
+					</article>
+				</div>
+			</section>
+      <div data-bs-spy="scroll" data-bs-target="#one" data-bs-offset="0" tabindex="0">
+      <table id="fat" class="table table-secondary table-hover" >
+		<thead>
+			<tr class="table-dark">
+				<th>N0</th>
+				<th>Nama Mitra Logistic</th>
+				<th>Kontak</th>				
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>1</td>
+				<td>21 Express</td>
+				<td>021 - 22442121</td>
+			</tr>
+			<tr>
+				<td>2</td>
+				<td>Acommerce</td>
+				<td>+62 21 2955 8500</td>
+			</tr>
+			<tr>
+				<td>3</td>
+				<td>Air Asia</td>
+				<td>help@teleport.asia</td>
+			</tr>
+			<tr>
+				<td>4</td>
+				<td>Als Cargo</td>
+				<td>+62 1235568583</td>
+			</tr>
+			<tr>
+				<td>5</td>
+				<td>Arifin Dirgantara Mandiri</td>
+				<td>(031) 3562236</td>
+			</tr>
+      <tr>
+				<td>6</td>
+				<td>Bali Prima</td>
+				<td>(031) 5027999</td>
+			</tr>
+			<tr>
+      <tr>
+				<td>7</td>
+				<td>Cardig</td>
+				<td>sales@cardig-express.com</td>
+			</tr>
+			<tr>
+      <tr>
+				<td>8</td>
+				<td>Cargonesia</td>
+				<td>(021) 53665404</td>
+			</tr>
+			<tr>
+      <tr>
+				<td>9</td>
+				<td>Citilink</td>
+				<td>cargosales@citilink.co.id</td>
+			</tr>
+			<tr>
+      <tr>
+				<td>10</td>
+				<td>Dakota Cargo</td>
+				<td>(021) 824-329-76</td>
+			</tr>
+			<tr>
+		</tbody>
+	</table>
+ </div>
+ <a href="logistic.php" class="btn btn-secondary btn-lg" style="margin-bottom: 300px ; margin-left: 650px; margin-top: 50px  ">Back</a>
+ <div data-bs-spy="scroll" data-bs-target="#one" data-bs-offset="0" tabindex="0">
+ <div class="d-flex flex-row bd-highlight mb-3">
+ <div id="mdo" class="card p-2  mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img class="logo" src="image/1.png" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Costum Document Clearance</h5>
+        <p class="card-text"> 1. Tahap perizinan barang. Arex menyediakan layanan perizinan barang baik export melalui jalur laut maupun import dengan sistem terkomputerisasi.</p>
+       
+      </div>
+    </div>
+  </div>
+  </div>
+  <div id="mdo" class="card p-2 mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img class="logo" src="image/2.png" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Stevedoring dan Cargodoring </h5>
+        <p class="card-text"> 2. Tahap bongkar muat. Stevedoring dan cargodoring layanan bongkar yang didukung dengan peralatan bongkar muat sendiri seperti Crane, Forklift, Lashing Gear, Container Spreader, Spreader Bar untuk Pipa Baja, dll..</p>
+        
+      </div>
+    </div>
+  </div>
+</div>
+<div id="mdo" class="card p-2 mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img class="logo" src="image/3.png" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Trucking</h5>
+        <p class="card-text">3. Tahap Pengiriman. Trucking adalah layanan jasa pengiriman barang via darat menggunakan armada mobil dan truk antar kota.  trucking juga sering disebut dengan jasa cargo pengiriman via laut dan udara</p>
+       </div>
+    </div>
+  </div>
+</div>
+ </div>
+ </div>
+ <div data-bs-spy="scroll" data-bs-target="#one" data-bs-offset="0" tabindex="0">
+ <div class="d-flex flex-row bd-highlight mb-3">
+ <div id="mdo" class="card p-2  mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img class="logo" src="image/4.png" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Warehouse and Packaging</h5>
+        <p class="card-text">4. Tahap gudang dan pengemasan. menyimpan produk /persediaan di gudang serta informasi barang kemudian <br> proses selanjutnya adalah pengemasan.</p>
+       </div>
+    </div>
+  </div>
+  </div>
+  <div id="mdo" class="card p-2 mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img class="logo" src="image/5.png" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Shipping Agent and Shipping Chartering</h5>
+        <p class="card-text">5. Tahap Shipping Agent and Shipping Chartering. kesepakatan melakukan pengangkutan cargo ke lokasi dan waktu tertentu.</p>
+        </div>
+    </div>
+  </div>
+</div>
+<div id="mdo" class="card p-2 mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img class="logo" src="image/6.png" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Freight Forwarding </h5>
+        <p class="card-text">6. Tahap pengangkutan barang.<br> Tahap terkahir merupakan layanan pengangkutan barang atau komoditas dari pelabuhan atau terminal sampai ke importir.</p>
+        </div>
+    </div>
+  </div>
+</div>
+ </div>
+ </div>
+
+
+ <footer class="bg text-white text-center text-lg-start">
+  <!-- Grid container -->
+  <div class="container p-4">
+    <!--Grid row-->
+    <div class="row">
+      <!--Grid column-->
+      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Footer Content</h5>
+
+        <p>
+        AREX merupakan sistem informasi yang bergerak dalam bidang bidang maritim untuk mempermudah pekerjaan yang ada di pelabuhan dengan sistem yang terkomputerisasi sehingga dapat mempersingkat waktu pengerjaan serta mempermudah user untuk mengetahu informasi-informasi mengenai bidang maritim.
+       
+        </p>
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Menu</h5>
+
+        <ul class="list-unstyled mb-0">
+          <li>
+            <a href="#!" class="text-white">Home</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">About us</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">Contact us</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">Help</a>
+          </li>
+        </ul>
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase mb-0">Find us on</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!" class="text-white"><img src="images/22.png" style="width:30px; height:30px;" alt="">@arga_executor</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white"><img src="images/23.png" style="width:30px; height:30px;" alt="">@arex_00</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white"><img src="images/24.png" style="width:30px; height:30px;" alt="">Arex</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white"><img src="images/25.png" style="width:30px; height:30px;" alt="">arex@gmail.com</a>
+          </li>
+        </ul>
+      </div>
+      <!--Grid column-->
+    </div>
+    <!--Grid row-->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: #D5D5D5;" >
+    
+    <p class="footer">© 2020 Copyright:ArgaExcecutor.com</p>
+  </div>
+  <!-- Copyright -->
+</footer>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+
+</body>
+</html>
